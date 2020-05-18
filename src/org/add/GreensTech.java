33e878c352dde38e6ef614ef01955f96699fca40
vijay-41196 <vijaymech41196@gmail.com>
2020-05-18 07:10:17 +0530

    comments

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensTech {
+public void greensOmr() {
+	System.out.println("omr branch");
+}
+public void greensAdayar() {
+	System.out.println("adayar branch");
+}
+public void greensTambaram() {
+	System.out.println("tambaram branch");
+}
+public void greensVelacherry() {
+	System.out.println("velacherry branch");
+}
+public void greensAnnanagar() {
+	System.out.println("annanagar branch");
+}
+public static void main(String[] args) {
+	GreensTech g=new GreensTech();
+	g.greensOmr();
+	g.greensAdayar();
+	g.greensTambaram();
+	g.greensVelacherry();
+	g.greensAnnanagar();
+}
+}
